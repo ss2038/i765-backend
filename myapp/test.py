e20d9244659cca1f2c0b152fa271e09a36da8f21
ss2038 <166275035+ss2038@users.noreply.github.com>
2024-04-08 07:24:40 -0500

    Update test.py

--- myapp/test.py
+++ myapp/test.py
@@ -4,7 +4,7 @@ from PyPDF2 import PdfReader, PdfWriter
 
     
 
-input_pdf_path = '/Users/seemandharjain/shefali/myproject/myapp/Form I-765, Application For Employment Authorization.pdf'
+input_pdf_path = '/Users/shefali/myproject/myapp/Form I-765, Application For Employment Authorization.pdf'
 output_pdf = io.BytesIO()
 
 with open(input_pdf_path, 'rb') as input_pdf:
@@ -12,21 +12,18 @@ with open(input_pdf_path, 'rb') as input_pdf:
     fields = reader.get_form_text_fields()
     writer = PdfWriter()
 
-    # Handle the first page
+    # Handle the first page of the form
     page_1_fields = {
-        # Include fields from the start till '4.c'
-        'givenName': 'Sam',
-        'familyName': 'Jain',
-        # Add other fields up to '4.c'
+        'givenName': 'Shefali',
+        'familyName': 'Saxena',
     }
     print(page_1_fields)
     page = reader.pages[0]  # First page
     writer.add_page(page)
     writer.update_page_form_field_values(writer.pages[0], page_1_fields)
 
-    # For any remaining pages, add them without modifications
     for i in range(1, len(reader.pages)):
         writer.add_page(reader.pages[i])
 
-with open("f1illed-out.pdf", "wb") as output_stream:
+with open("filled-out.pdf", "wb") as output_stream:
     writer.write(output_stream)
